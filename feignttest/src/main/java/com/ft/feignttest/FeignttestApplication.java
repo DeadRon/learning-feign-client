e2f62a21a4cc2b5dcce358fe202caf77d0807971
Ronaldo Melo <ronaldorpm1994@gmail.com>
2022-09-08 00:27:20 -0300

    enable feing client

--- feignttest/src/main/java/com/ft/feignttest/FeignttestApplication.java
+++ feignttest/src/main/java/com/ft/feignttest/FeignttestApplication.java
@@ -1,3 +1,5 @@
+package com.ft.feignttest;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
